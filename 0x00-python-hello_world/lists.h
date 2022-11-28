843339e85e21b1c8cd5bcc1771cc737a6e54b2d4
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-11-28 12:26:56 -0800

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,19 +2,21 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
-* struct listint_s - singly linked list.
-* @n: integer.
-* @next: points to the next node.
-*
-* Description: singly linked list node structure
-* for Holberton project.
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
