4eb8fadf3a7c00113173dcaef642c1befecead14
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-11-29 10:48:03 -0800

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
-    if (i % 2 == 0):
-        print('{:c}'.format(i), end='')
-    else:
-        print('{:c}'.format(i - 32), end='')
+# Author -Bamidele Adefolaju
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
