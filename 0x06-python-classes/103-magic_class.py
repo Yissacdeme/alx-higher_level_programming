b4081cb62b92574aaa03687919aaffa813417015
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-12-21 10:15:19 +1300

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""writing a docstring"""
+"""Define a MagicClass that does exactly as the bytecode provided."""
+
 import math
 
 
 class MagicClass:
-    """set up the magic"""
+    """Represent a circle."""
 
     def __init__(self, radius=0):
-        """ writing another docstring """
+        """Initialize a MagicClass.
+        
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """again with the docstring"""
-        return self.__radius ** 2 * math.pi
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """such docstring"""
-        return 2 * math.pi * self.
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
