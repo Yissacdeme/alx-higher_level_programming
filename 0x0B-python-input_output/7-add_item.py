a9fa4d17a0cfb89170a835a0cb9f4298be86961b
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-01-11 05:04:47 +1300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
 import sys
 
 if __name__ == "__main__":
